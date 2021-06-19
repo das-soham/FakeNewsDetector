34bbafe0fa9c8f79ecbeb23b4ce8d141386a13ff
Soham Das <sohamdas@sohamdas.me>
2021-06-19 23:59:09 +0800

    Dec v 0.05: Modifying datasetstreamer

--- datastream.py
+++ datastream.py
@@ -1,24 +1,43 @@
+import numpy
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, IterableDataset
 import pandas as pd
 import os
+from itertools import cycle
+
 
 class NewsProducer(Dataset):
-    def __init__(self,path, filename):
-        self.data = pd.read_csv(os.path.join(path,filename), delimiter = ',', error_bad_lines=False)
+    def __init__(self, path, filename):
+        self.data = pd.read_csv(os.path.join(path, filename), delimiter=',', error_bad_lines=False)
+
     def __len__(self):
         return (len(self.data))
+
     def __getitem__(self, item):
         if torch.is_tensor(item):
             item = item.tolist()
         news = self.data.loc[item, 'text']
         label = self.data.loc[item, 'Label']
-        return news,label
+        return news, label
 
 
-training_dataset = NewsProducer('data', 'Train.csv')
+class IterableNewsProducer(IterableDataset):
+    def __init__(self, path, filename):
+        super(IterableNewsProducer).__init__()
+        self.data = pd.read_csv(os.path.join(path, filename), delimiter=',', error_bad_lines=False)
 
-torch.manual_seed(1)
-train_loader = DataLoader(training_dataset,batch_size=10,shuffle=True, num_workers=0)
+    def datastream(self):
+        for i in self.data.index:
+            news = self.data.loc[i,'text']
+            label = self.data.loc[i, 'Label']
+            yield from (news, label)
+    def __iter__(self):
+        return self.datastream()
+
+
+
+
+
+
 
 
